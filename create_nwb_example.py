2d9df3b0acea07a8e1f00095b844bbdb5d0bd243
Loren Frank <loren@phy.ucsf.edu>
2020-08-14 08:44:06 -0700

    Additions to nwb creation script and update to despereaux metadata to include task epochs for associated files

--- create_nwb_example.py
+++ create_nwb_example.py
@@ -10,24 +10,27 @@ def main():
     print('Setting variables', flush=True)
    
     # set the animal name and the date or list of dates to process
-    animal_name = 'beans'
-    dates = ['20190718']
-    animal_metadata_file = 'beans20190718_metadata.yml'
+    #animal_name = 'beans'
+    #date = '20190718'
+    #animal_metadata_file = 'beans20190718_metadata.yml'
+    #reconfig_file = '/Users/loren/data/nwb_builder_test_data/beans/Probe_128ch_allnT_DIOs_PTP_reconfig_export_shanks.xml'
 
-    # animal_name = 'despereaux'
-    # date = '20191125'
+    animal_name = 'despereaux'
+    date = '20191125'
+    animal_metadata_file = 'despereaux20191125.yml'
+    reconfig_file = '/stelmo/loren/despereaux/raw/20191125/20191125_despereaux_01_s1.rec_header.xml'
 
-    yaml_path = '/Users/loren/Src/NWB/franklabnwb/yaml'
+    yaml_path = '/home/loren/Src/NWB/franklabnwb/yaml'
     probe1_metadata_file = '128c-4s8mm6cm-20um-40um-sl.yml'
     probe2_metadata_file = 'tetrode_12.5.yml'
 
     #Specify the paths for the data, the output nwb file, and the video files
-    data_path = '/Users/loren/data/nwb_builder_test_data/'
-    output_path='/Users/loren/data/nwb_builder_test_data/'
-    video_path='/Users/loren/data/nwb_builder_test_data/video/'
+    data_path = '/stelmo/loren/'
+    output_path='/stelmo/nwb/'
+    video_path='/stelmo/nwb/video/'
 
     # Specify any optional trodes export flags
-    trodes_rec_export_args = ('-reconfig', '/Users/loren/data/nwb_builder_test_data/beans/Probe_128ch_allnT_DIOs_PTP_reconfig_export_shanks.xml') 
+    trodes_rec_export_args = ('-reconfig', reconfig_file) 
 
 
     # specify the locations of the metadata files for the animal and the probe(s). 
@@ -55,13 +58,13 @@ def main():
     print('Creating Builder', flush=True)
     builder = RawToNWBBuilder(animal_name=animal_name,
                           data_path=data_path,
-                          dates=dates,
+                          dates=[date],
                           nwb_metadata=metadata,
                           overwrite=overwrite,
                           output_path=output_path,
                           video_path=video_path,
-                          extract_analog=True)
-#                         trodes_rec_export_args = trodes_rec_export_args)
+                          extract_analog=True,
+                          trodes_rec_export_args = trodes_rec_export_args)
 
     print('Building and Writing', flush=True)
     builder.build_nwb(process_mda_valid_time='False',
